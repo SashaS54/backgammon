dd0b8c16f1aabc33090b88f51571e8b8214de1c8
sasha <sodyl.ssha@gmail.com>
2023-08-07 17:45:31 +0200

    Empty window

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import pygame
+
+
 def main():
-    print("Hello world!")
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    close = False
+
+    while not close:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                close = True
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
