eebd6296ea04c5b49566954a4191bb77739e43bc
sasha <sodyl.ssha@gmail.com>
2023-08-08 14:45:44 +0200

    Improved typing in Color class and added toTuple method

--- color.py
+++ color.py
@@ -1,11 +1,17 @@
+from typing import Tuple
+
+
 class Color:
     def __init__(self, r: int, g: int, b: int, a: int = 255):
         assert r <= 255 and g <= 255 and b <= 255 and a <= 255
 
-        self.r = r
-        self.g = g
-        self.b = b
-        self.a = a
+        self.r: int = r
+        self.g: int = g
+        self.b: int = b
+        self.a: int = a
+
+    def toTuple(self) -> Tuple[int, int, int, int]:
+        return self.r, self.g, self.b, self.a
 
 
 Color.Red = Color(255, 0, 0)
