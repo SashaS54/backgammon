b0eac118f76292e910c19f4801afc5650587f822
sasha <sodyl.ssha@gmail.com>
2023-08-09 14:56:41 +0200

    Moved some variables to constants.py

--- gameField.py
+++ gameField.py
@@ -11,42 +11,38 @@ class GameField:
         self.leftField: Rectangle = self.createField(False)
         self.rightField: Rectangle = self.createField(True)
 
-        self.__bordersWidth: float = self.leftField.width * constants.BORDER_WIDTH_PERCENT
         self.leftFieldBorders: Tuple[Rectangle, Rectangle, Rectangle, Rectangle] = self.createFieldBorders(self.leftField)
         self.rightFieldBorders: Tuple[Rectangle, Rectangle, Rectangle, Rectangle] = self.createFieldBorders(self.rightField)
 
-        self.__trianglesHeight: float = (self.leftField.height - 2 * self.__bordersWidth) * 0.3
-        self.__trianglesBaseLength: float = (self.leftField.width + self.rightField.width
-                                             - 4 * self.__bordersWidth) / 12
         self.triangles: List[Polygon] = [self.createTriangle(i) for i in range(24)]
 
         self.thatCubeShit: Rectangle = Rectangle(pygame.Vector2(constants.WINDOW_WIDTH * 0.9, 0),
                                                  constants.WINDOW_WIDTH * 0.1, constants.WINDOW_HEIGHT, Color.Blue)
 
     def createField(self, isRight: bool) -> Rectangle:
-        return Rectangle(pygame.Vector2(constants.WINDOW_WIDTH * 0.45 * isRight, 0), constants.WINDOW_WIDTH * 0.45,
+        return Rectangle(pygame.Vector2(constants.FIELD_WIDTH * isRight, 0), constants.FIELD_WIDTH,
                          constants.WINDOW_HEIGHT, Color.Red if isRight else Color.Green)  # TODO Change to same color
 
     def createFieldBorders(self, field: Rectangle) -> Tuple[Rectangle, Rectangle, Rectangle, Rectangle]:
         return (Rectangle(pygame.Vector2(field.point.x, field.point.y),
-                          self.__bordersWidth, field.height, Color.BorderColor),
+                          constants.BORDER_WIDTH, field.height, Color.BorderColor),
                 Rectangle(pygame.Vector2(field.point.x, field.point.y),
-                          field.width, self.__bordersWidth, Color.BorderColor),
-                Rectangle(pygame.Vector2(field.point.x + field.width - self.__bordersWidth, field.point.y),
-                          self.__bordersWidth, field.height, Color.BorderColor),
-                Rectangle(pygame.Vector2(field.point.x, field.point.y + field.height - self.__bordersWidth),
-                          field.width, self.__bordersWidth, Color.BorderColor))
+                          field.width, constants.BORDER_WIDTH, Color.BorderColor),
+                Rectangle(pygame.Vector2(field.point.x + field.width - constants.BORDER_WIDTH, field.point.y),
+                          constants.BORDER_WIDTH, field.height, Color.BorderColor),
+                Rectangle(pygame.Vector2(field.point.x, field.point.y + field.height - constants.BORDER_WIDTH),
+                          field.width, constants.BORDER_WIDTH, Color.BorderColor))
 
     def createTriangle(self, index: int) -> Polygon:
         side: bool = False if index < 12 else True
-        offset: float = 2 * self.__bordersWidth if 5 < index < 12 or index > 17 else 0
-        triangleHeight: float = self.__trianglesHeight + self.__bordersWidth if not side\
-            else constants.WINDOW_HEIGHT - self.__trianglesHeight - self.__bordersWidth
-        yBasePos: float = constants.WINDOW_HEIGHT * side + self.__bordersWidth * (1 - 2 * side)
+        offset: float = 2 * constants.BORDER_WIDTH if 5 < index < 12 or index > 17 else 0
+        triangleHeight: float = constants.TRIANGLES_HEIGHT + constants.BORDER_WIDTH if not side\
+            else constants.WINDOW_HEIGHT - constants.TRIANGLES_HEIGHT - constants.BORDER_WIDTH
+        yBasePos: float = constants.WINDOW_HEIGHT * side + constants.BORDER_WIDTH * (1 - 2 * side)
 
-        points: List[pygame.Vector2] = [pygame.Vector2(self.__bordersWidth + self.__trianglesBaseLength * (index % 12) + offset, yBasePos),
-                                        pygame.Vector2(self.__bordersWidth + self.__trianglesBaseLength * (index % 12 + 1) + offset, yBasePos),
-                                        pygame.Vector2(self.__bordersWidth + self.__trianglesBaseLength * (index % 12 + 0.5) + offset,
+        points: List[pygame.Vector2] = [pygame.Vector2(constants.BORDER_WIDTH + constants.TRIANGLES_BASE_LENGTH * (index % 12) + offset, yBasePos),
+                                        pygame.Vector2(constants.BORDER_WIDTH + constants.TRIANGLES_BASE_LENGTH * (index % 12 + 1) + offset, yBasePos),
+                                        pygame.Vector2(constants.BORDER_WIDTH + constants.TRIANGLES_BASE_LENGTH * (index % 12 + 0.5) + offset,
                                                        triangleHeight)]
 
         return Polygon(points, Color(15, 15, 15) if index % 2 == 1 else Color(71, 71, 71))
