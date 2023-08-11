20e3be64b3a7234a9109780a7bb443eeeac09fe6
sasha <sodyl.ssha@gmail.com>
2023-08-11 19:48:55 +0200

    Simplified points calculation function

--- triangle.py
+++ triangle.py
@@ -13,14 +13,14 @@ class Triangle:
 
     def calculateGeometryPoints(self) -> Tuple[pygame.Vector2, pygame.Vector2, pygame.Vector2]:
         side: bool = False if self.index < 12 else True
-        offset: float = 2 * constants.BORDER_WIDTH if 5 < self.index < 12 or self.index > 17 else 0
+        borderOffset: float = constants.BORDER_WIDTH + (constants.BORDER_WIDTH * 2 * (5 < self.index < 12 or self.index > 17))
         triangleHeight: float = constants.TRIANGLES_HEIGHT + constants.BORDER_WIDTH if not side \
             else constants.WINDOW_HEIGHT - constants.TRIANGLES_HEIGHT - constants.BORDER_WIDTH
         yBasePos: float = constants.WINDOW_HEIGHT * side + constants.BORDER_WIDTH * (1 - 2 * side)
 
-        return (pygame.Vector2(constants.BORDER_WIDTH + constants.TRIANGLES_BASE_LENGTH * (self.index % 12) + offset, yBasePos),
-                pygame.Vector2(constants.BORDER_WIDTH + constants.TRIANGLES_BASE_LENGTH * (self.index % 12 + 1) + offset, yBasePos),
-                pygame.Vector2(constants.BORDER_WIDTH + constants.TRIANGLES_BASE_LENGTH * (self.index % 12 + 0.5) + offset, triangleHeight))
+        return (pygame.Vector2(borderOffset + constants.TRIANGLES_BASE_LENGTH * (self.index % 12), yBasePos),
+                pygame.Vector2(borderOffset + constants.TRIANGLES_BASE_LENGTH * (self.index % 12 + 1), yBasePos),
+                pygame.Vector2(borderOffset + constants.TRIANGLES_BASE_LENGTH * (self.index % 12 + 0.5), triangleHeight))
 
     def render(self, surface: pygame.Surface):
         self._geometry.render(surface)
