b3dc3253a97cbbf56b43f9908bdf9c0636e00134
sasha <sodyl.ssha@gmail.com>
2023-08-29 19:22:44 +0200

    Mamu jebal

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--load")
     args = parser.parse_args()
-    print(args)
 
     Application(args.load).start()
